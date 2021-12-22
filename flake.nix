112c4c5d5af05c663725f764374a4943c05925be
Chris Pickard <chrispickard9@gmail.com>
2021-12-22 12:29:30 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in rec {
+        # `nix build`
+        packages.oxidized-endlessh = naersk-lib.buildPackage {
+          pname = "oxidized-endlessh";
+          root = ./.;
+        };
+        defaultPackage = packages.oxidized-endlessh;
+
+        # `nix run`
+        apps.oxidized-endlessh =
+          utils.lib.mkApp { drv = packages.oxidized-endlessh; };
+        defaultApp = apps.oxidized-endlessh;
+
+        # `nix develop`
+        devShell =
+          pkgs.mkShell { nativeBuildInputs = with pkgs; [ rustc cargo ]; };
+      });
+}
